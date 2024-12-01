1a2c44e11c9ae8a5fb9fd11a1792bec2af2bb167
giocri <67639762+giocri@users.noreply.github.com>
2024-12-01 19:33:53 +0100

    simd_approach

--- src/day1.rs
+++ src/day1.rs
@@ -1,5 +1,7 @@
 use std::fmt::Display;
 
+use wide::{i32x4, CmpGt, CmpLt};
+
 pub fn part1(input: &str) -> impl Display {
     let mut vec1 = Vec::new();
     let mut vec2 = Vec::new();
@@ -19,6 +21,8 @@ pub fn part1(input: &str) -> impl Display {
 pub fn part2(input: &str) -> impl Display {
     let mut vec1 = Vec::new();
     let mut vec2 = Vec::new();
+    vec1.reserve(10000);
+    vec2.reserve(10000);
     for line in input.lines() {
         let mut row = line.split_ascii_whitespace();
         vec1.push(row.next().unwrap().parse::<i32>().unwrap());
@@ -29,12 +33,45 @@ pub fn part2(input: &str) -> impl Display {
     let mut out = 0;
     let mut progress = 0;
     for left in vec1 {
-        while vec2[progress] < left {
-            progress += 1;
+        let check: i32x4 = left.into();
+        'prefix: {
+            while progress < vec2.len() - 3 {
+                let block = i32x4::new([
+                    vec2[progress],
+                    vec2[progress + 1],
+                    vec2[progress + 2],
+                    vec2[progress + 2],
+                ]);
+                let count = check.cmp_gt(block).reduce_add();
+                progress -= count as usize; //sign is inverted
+                if count > -4 {
+                    break 'prefix;
+                }
+            }
+            while vec2[progress] < left {
+                progress += 1;
+            }
         }
-        while vec2[progress] == left {
-            out += left;
-            progress += 1;
+        'count: {
+            while progress < vec2.len() - 3 {
+                let block = i32x4::new([
+                    vec2[progress],
+                    vec2[progress + 1],
+                    vec2[progress + 2],
+                    vec2[progress + 2],
+                ]);
+                let check: i32x4 = left.into();
+                let count = 4 + check.cmp_lt(block).reduce_add();
+                progress += count as usize;
+                out += count * left;
+                if count < 4 {
+                    break 'count;
+                }
+            }
+            while vec2[progress] < left {
+                progress += 1;
+                out += left;
+            }
         }
     }
     out
