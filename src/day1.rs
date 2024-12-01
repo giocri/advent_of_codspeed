eadee5366d3f95168b17e9ec2dd4493bc744b676
giocri <67639762+giocri@users.noreply.github.com>
2024-12-01 19:59:15 +0100

    bugfix and per test

--- src/day1.rs
+++ src/day1.rs
@@ -1,10 +1,12 @@
 use std::fmt::Display;
 
-use wide::{i32x4, CmpGt, CmpLt};
+use wide::{i32x4, i32x8, CmpGt, CmpLt};
 
 pub fn part1(input: &str) -> impl Display {
     let mut vec1 = Vec::new();
     let mut vec2 = Vec::new();
+    vec1.reserve(10000);
+    vec2.reserve(10000);
     for line in input.lines() {
         let mut row = line.split_ascii_whitespace();
         vec1.push(row.next().unwrap().parse::<i32>().unwrap());
@@ -33,18 +35,22 @@ pub fn part2(input: &str) -> impl Display {
     let mut out = 0;
     let mut progress = 0;
     for left in vec1 {
-        let check: i32x4 = left.into();
+        let check: i32x8 = left.into();
         'prefix: {
-            while progress < vec2.len() - 3 {
-                let block = i32x4::new([
+            while progress < vec2.len() - 7 {
+                let block = i32x8::new([
                     vec2[progress],
                     vec2[progress + 1],
                     vec2[progress + 2],
-                    vec2[progress + 2],
+                    vec2[progress + 3],
+                    vec2[progress + 4],
+                    vec2[progress + 5],
+                    vec2[progress + 6],
+                    vec2[progress + 7],
                 ]);
                 let count = check.cmp_gt(block).reduce_add();
                 progress -= count as usize; //sign is inverted
-                if count > -4 {
+                if count > -8 {
                     break 'prefix;
                 }
             }
@@ -58,7 +64,7 @@ pub fn part2(input: &str) -> impl Display {
                     vec2[progress],
                     vec2[progress + 1],
                     vec2[progress + 2],
-                    vec2[progress + 2],
+                    vec2[progress + 3],
                 ]);
                 let check: i32x4 = left.into();
                 let count = 4 + check.cmp_lt(block).reduce_add();
