e95adfef3e7ca8424bb1f19e01845c41958a33e4
giocri <67639762+giocri@users.noreply.github.com>
2024-12-01 11:17:10 +0100

    day1

---
+++ src/day1.rs
@@ -0,0 +1,18 @@
+use std::fmt::Display;
+
+pub fn part1(input: &str) -> impl Display {
+    let mut vec1 = Vec::new();
+    let mut vec2 = Vec::new();
+    for line in input.lines() {
+        let mut row = line.split_ascii_whitespace();
+        vec1.push(row.next().unwrap().parse::<i32>().unwrap());
+        vec2.push(row.next().unwrap().parse::<i32>().unwrap());
+    }
+    vec1.sort();
+    vec2.sort();
+    let mut out = 0;
+    for i in 0..vec1.len() {
+        out += (vec1[i] - vec2[i]).abs();
+    }
+    out
+}
