6057358d231926f13359af7a58faa4c7f2788a08
giocri <67639762+giocri@users.noreply.github.com>
2024-12-01 11:35:09 +0100

    part2

--- src/day1.rs
+++ src/day1.rs
@@ -16,3 +16,26 @@ pub fn part1(input: &str) -> impl Display {
     }
     out
 }
+pub fn part2(input: &str) -> impl Display {
+    let mut vec1 = Vec::new();
+    let mut vec2 = Vec::new();
+    for line in input.lines() {
+        let mut row = line.split_ascii_whitespace();
+        vec1.push(row.next().unwrap().parse::<i32>().unwrap());
+        vec2.push(row.next().unwrap().parse::<i32>().unwrap());
+    }
+    vec1.sort();
+    vec2.sort();
+    let mut out = 0;
+    let mut progress = 0;
+    for left in vec1 {
+        while vec2[progress] < left {
+            progress += 1;
+        }
+        while vec2[progress] == left {
+            out += left;
+            progress += 1;
+        }
+    }
+    out
+}
