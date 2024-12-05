412b75d6c45601e5745db4336eb6109061d49e38
giocri <67639762+giocri@users.noreply.github.com>
2024-12-05 13:07:55 +0100

    day5

---
+++ src/day5.rs
@@ -0,0 +1,70 @@
+use std::{cmp, collections::HashMap, fmt::Display};
+const CONSTRAINTS: i32 = 1176;
+const UPDATES: i32 = 1377 - 1177;
+pub fn part2(input: &str) -> impl Display {
+    let mut constraints_map: Vec<Vec<usize>> = vec![Vec::new(); 100];
+    let mut lines = input.lines();
+    for _ in 0..CONSTRAINTS {
+        let line = lines.next().unwrap();
+        constraints_map[u32::from_str_radix(line[0..2].as_ref(), 10).unwrap() as usize]
+            .push(u32::from_str_radix(line[3..5].as_ref(), 10).unwrap() as usize);
+    }
+    let mut out = 0;
+    /*for i in 0..100 {
+        constraints_map.sort();
+    }*/
+    lines.next().unwrap();
+    for _ in 0..UPDATES {
+        let line = lines.next().unwrap();
+        let mut pages: Vec<u32> = line
+            .split(',')
+            .map(|f| u32::from_str_radix(f, 10).unwrap())
+            .collect();
+        let old = pages.clone();
+        pages.sort_by(|&a, &b| {
+            if constraints_map[a as usize].contains(&(b as usize)) {
+                cmp::Ordering::Less
+            } else if constraints_map[b as usize].contains(&(a as usize)) {
+                cmp::Ordering::Greater
+            } else {
+                cmp::Ordering::Equal
+            }
+        });
+        if pages != old {
+            out += pages[pages.len() / 2];
+        }
+    }
+    out
+}
+pub fn part1(input: &str) -> impl Display {
+    let mut constraints_map: Vec<Vec<usize>> = vec![Vec::new(); 100];
+    let mut lines = input.lines();
+    for _ in 0..CONSTRAINTS {
+        let line = lines.next().unwrap();
+        constraints_map[u32::from_str_radix(line[0..2].as_ref(), 10).unwrap() as usize]
+            .push(u32::from_str_radix(line[3..5].as_ref(), 10).unwrap() as usize);
+    }
+    let mut out = 0;
+    lines.next().unwrap();
+    for _ in 0..UPDATES {
+        let line = lines.next().unwrap();
+        let mut pages: Vec<u32> = line
+            .split(',')
+            .map(|f| u32::from_str_radix(f, 10).unwrap())
+            .collect();
+        let old = pages.clone();
+        pages.sort_by(|&a, &b| {
+            if constraints_map[a as usize].contains(&(b as usize)) {
+                cmp::Ordering::Less
+            } else if constraints_map[b as usize].contains(&(a as usize)) {
+                cmp::Ordering::Greater
+            } else {
+                cmp::Ordering::Equal
+            }
+        });
+        if pages == old {
+            out += pages[pages.len() / 2];
+        }
+    }
+    out
+}
